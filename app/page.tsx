7505a691a7d4bea7e67321d8bce11d9def194311
Yaroslav Samoylov <yaosamo@gmail.com>
2023-11-09 10:05:34 -0800

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   const handleKeyDown = (event) => {
     setPressedKey(event.key); // Set the pressed key to the state
   };
-
   useEffect(() => {
     // Attach the event listener to the window object
     window.addEventListener("keydown", handleKeyDown);
@@ -22,9 +21,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="font-mono text-sm">
         <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Esc {pressedKey}
+          Pressed key: {pressedKey}
         </div>
       </div>
     </main>
